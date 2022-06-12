4688688faff1dd1171c0a12214811378a66b6bf9
khayratdinov <khayratdinov.np@gmail.com>
2022-06-12 10:28:38 +0500

    feat: add custom user model

---
+++ apps/users/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+# ============================================================================ #
+from .models import User
+# ============================================================================ #
+
+
+# ============================= CREATE USER FORM ============================= #
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = User
+        fields = ["email", "username", "first_name", "last_name"]
+        error_class = "error"
+
+
+# ============================= CHANGE USER FORM ============================= #
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ["email", "username", "first_name", "last_name"]
+        error_class = "error"
--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+# ============================================================================ #
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
+# ================================ ADMIN PANEL =============================== #
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Portal"
