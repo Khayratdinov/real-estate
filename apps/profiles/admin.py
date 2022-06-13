050fbd73abb20eced07cab7c02cf9c063dbfc4b2
khayratdinov <khayratdinov.np@gmail.com>
2022-06-13 11:51:47 +0500

    feat: add profile model and signal

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+# ============================================================================ #
+from .models import Profile
 
-# Register your models here.
+
+
+# =============================== PROFILEADMIN =============================== #
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["id", "pkid", "user", "gender", "phone_number", "country", "city"]
+    list_filter = ["gender", "country", "city"]
+    list_display_links = ["id", "pkid", "user"]
+
+
+
+# ============================================================================ #
+admin.site.register(Profile, ProfileAdmin)
