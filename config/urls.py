3db313d100269576eab5433f1fd74f3dd0d8d75b
khayratdinov <khayratdinov.np@gmail.com>
2022-06-13 14:04:03 +0500

    feat: user auth-register, login, password, reset

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
